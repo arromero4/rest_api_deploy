74685e12bfd7787e02c072d10726468cf5175ada
Andrés Reyes Romero <arromero0408@gmail.com>
2026-06-08 00:19:12 -0600

    feat: add new changes

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require('express'); //commonJS
-const movies = require('./movies.json');
-const crypto = require('crypto');
-const cors = require('cors');
-const { validateMovie, validatePartialMovie } = require('./schemas/movies.js');
+import express, { json } from 'express'; //ES Modules
+import movies, { filter, find, push, findIndex, splice } from './movies.json';
+import { randomUUID } from 'crypto';
+import cors from 'cors';
+import { validateMovie, validatePartialMovie } from './schemas/movies.js';
 
 const PORT = process.env.PORT || 1234;
 const app = express();
-app.use(express.json());    
+app.use(json());    
 app.disable('x-powered-by');
 app.use(cors({
     origin: (origin, callback) => {
@@ -55,7 +55,7 @@ app.get('/movies', (req, res) => {
 
     const { genre } = req.query;
     if (genre) {
-        const filteredMovies = movies.filter(movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase()));
+        const filteredMovies = filter(movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase()));
 
         return res.json(filteredMovies);
     }
@@ -65,7 +65,7 @@ app.get('/movies', (req, res) => {
 
 app.get('/movies/:id', (req, res) => {
     const { id } = req.params;
-    const movie = movies.find(movie => movie.id === id);
+    const movie = find(movie => movie.id === id);
 
     if (movie) return res.json(movie);
     res.status(404).json({ message: 'Movie not found' });
@@ -82,11 +82,11 @@ app.post('/movies', (req, res) => {
 
     //base de datos 
     const newMovie = {
-        id: crypto.randomUUID(),
+        id: randomUUID(),
         ...result.data,
 
     }
-    movies.push(newMovie);
+    push(newMovie);
     res.status(201).json(newMovie);
 })
 
@@ -100,10 +100,10 @@ app.delete('/movies/:id', (req, res) => {
 
 
     const { id } = req.params;
-    const movieIndex = movies.findIndex(movie => movie.id === id);
+    const movieIndex = findIndex(movie => movie.id === id);
     if(movieIndex === -1) return res.status(404).json({ message: 'Movie not found' });
 
-    movies.splice(movieIndex, 1);
+    splice(movieIndex, 1);
     return res.json({ message: 'Movie deleted successfully' });
 })
 
@@ -114,7 +114,7 @@ app.patch('/movies/:id', (req, res) => {
     if(!result.success){
         return res.status(400).json({ error: JSON.parse(result.error.message) });
     }
-    const movieIndex = movies.findIndex(movie => movie.id === id);
+    const movieIndex = findIndex(movie => movie.id === id);
     if(movieIndex === -1) return res.status(404).json({ message: 'Movie not found' });
 
     const updatedMovie = {
